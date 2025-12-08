fab5993e7bd1697a3c13d236631a9394bfb9643c
veyne <a1622373589@outlook.com>
2025-12-08 12:44:42 +0800

    feat: add base address rigister

--- hdc/pci.cpp
+++ hdc/pci.cpp
@@ -60,7 +60,7 @@ bool PeripheralComponentInterconnectController::DeviceHasFunctions(uint16_t bus,
     return Read(bus, device, 0, 0x0E) & (1<<7);
 }
 
-void PeripheralComponentInterconnectController::SelectDrivers(DriverManager *driverManager)
+void PeripheralComponentInterconnectController::SelectDrivers(DriverManager *driverManager, InterruptManager *interrupts)
 {
     for(int bus = 0; bus < 8; bus++) {
         for(int device = 0; device < 32; device++) {
@@ -70,6 +70,16 @@ void PeripheralComponentInterconnectController::SelectDrivers(DriverManager *dri
 
                 if(dev.vendor_id == 0x0000 || dev.vendor_id == 0xFFFF) {
                     continue;
+                }
+                for(int barNum = 0; barNum < 6; barNum++) {
+                    BaseAddressRegister bar = GetBaseAddressRegister(bus, device, function, barNum);
+                    if(bar.address && (bar.type == InputOutput)) {
+                        dev.portBase = (uint32_t)bar.address;
+                    }
+                    Driver *driver = GetDriver(dev, interrupts);
+                    if(driver != 0) {
+                        driverManager->AddDriver(driver);
+                    }
                 }
                 Kernel::printf("PCI BUS ");
                 Kernel::printfHex(bus & 0xFF);
@@ -91,6 +101,56 @@ void PeripheralComponentInterconnectController::SelectDrivers(DriverManager *dri
             }
         }
     }
+}
+
+BaseAddressRegister PeripheralComponentInterconnectController::GetBaseAddressRegister(uint16_t bus, uint16_t device, uint16_t function, uint16_t bar)
+{
+    BaseAddressRegister result;
+    uint32_t headertype = Read(bus, device, function, 0x0E) & 0x7F;
+    int maxbars = 6 - (4*headertype);
+    if(bar >= maxbars) {
+        return result;
+    }
+    uint32_t bar_value = Read(bus, device, function, 0x10 + 4*bar);
+    result.type = (bar_value & 0x1) ? InputOutput : MemoryMapping;
+    uint32_t temp;
+    if(result.type == MemoryMapping) {
+        switch (((bar_value >> 1) && 0x3)) {
+            case 0: //32 BitMode;
+            case 1: //32 BitMode;
+            case 2: //32 BitMode;
+                break;
+        }
+    } else { //InputOutput
+        result.address = (uint8_t *)(bar_value & ~0x3);
+        result.prefetchable = false;
+    }
+    return result;
+}
+
+Driver *PeripheralComponentInterconnectController::GetDriver(PeripheralComponentInterconnectDeviceDesriptor dev, InterruptManager *interrupts)
+{
+    switch ((dev.vendor_id)) {
+        case 0x1022: //AMD
+            switch(dev.device_id) {
+                case 0x2000: //am79c973
+                    JLOS::Kernel::printf("AMD am79c973 ");
+                    break;
+            }
+            break;
+        case 0x8086: //Intel
+            break;
+    }
+    switch(dev.class_id) {
+        case 0x03: //graphics
+            switch(dev.subclass_id) {
+                case 0x00: //VGA
+                    JLOS::Kernel::printf("VGA ");
+                    break;
+            }
+            break;
+    }
+    return 0;
 }
 
 PeripheralComponentInterconnectDeviceDesriptor PeripheralComponentInterconnectController::GetDeviceDescriptor(uint16_t bus, uint16_t device, uint16_t function)
